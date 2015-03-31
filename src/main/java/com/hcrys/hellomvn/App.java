37b5a58602070998f82614e6b9a0b79b1f6ac67f
Phu Nguyen Anh <phuna24@gmail.com>
2015-04-01 06:25:38 +0700

    Initial commit

---
+++ src/main/java/com/hcrys/hellomvn/App.java
@@ -0,0 +1,13 @@
+package com.hcrys.hellomvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
